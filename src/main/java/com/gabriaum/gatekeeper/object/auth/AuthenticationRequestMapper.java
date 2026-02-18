6ac656a2c7c1bebfe221787875e4137e89fcee36
gabriaum <gaabrielbruck@gmail.com>
2026-02-18 18:45:40 -0300

    refactor: move CryptographyService.java to infra/service pkg

--- src/main/java/com/gabriaum/gatekeeper/object/auth/AuthenticationRequestMapper.java
+++ src/main/java/com/gabriaum/gatekeeper/object/auth/AuthenticationRequestMapper.java
@@ -1,6 +1,6 @@
 package com.gabriaum.gatekeeper.object.auth;
 
-import com.gabriaum.gatekeeper.infra.CryptographyService;
+import com.gabriaum.gatekeeper.infra.service.CryptographyService;
 import com.gabriaum.gatekeeper.object.auth.dto.AuthenticationLoginRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
