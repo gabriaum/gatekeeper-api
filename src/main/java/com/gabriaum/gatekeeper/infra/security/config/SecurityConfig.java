c2ec5a12527b7f7d5d4c12ae9bd825e49742aa49
gabriaum <gaabrielbruck@gmail.com>
2026-02-18 17:58:51 -0300

    feat: security config [allowing swagger request]

---
+++ src/main/java/com/gabriaum/gatekeeper/infra/security/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.gabriaum.gatekeeper.infra.security.config;
+
+import com.gabriaum.gatekeeper.infra.security.JwtAuthFilter;
+import com.gabriaum.gatekeeper.infra.security.LoginAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private LoginAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    private JwtAuthFilter filter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(
+                            "/swagger-ui/**",
+                            "/v3/api-docs/**",
+                            "/swagger-ui.html"
+                    ).permitAll()
+                .anyRequest().authenticated())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint))
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
