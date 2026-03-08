30c67a08c66ef10d6914ef5b721108ab988426bd
gabriaum <gaabrielbruck@gmail.com>
2026-03-08 17:46:51 -0300

    feat: add method in the user repository to check if there are any registered admin users

--- src/main/java/com/gabriaum/gatekeeper/object/user/repository/GateUserRepository.java
+++ src/main/java/com/gabriaum/gatekeeper/object/user/repository/GateUserRepository.java
@@ -1,6 +1,7 @@
 package com.gabriaum.gatekeeper.object.user.repository;
 
 import com.gabriaum.gatekeeper.object.user.GateUser;
+import com.gabriaum.gatekeeper.object.user.enums.GateUserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface GateUserRepository extends JpaRepository<GateUser, Long> {
     Optional<GateUser> findByEmail(String email);
     Optional<GateUser> findByCpf(String cpf);
+    Boolean existsByRole(GateUserRole role);
 }
