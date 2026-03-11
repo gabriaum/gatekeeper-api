6803de9b8b7e871d4aa47108cd57ebc7f5dfe01c
gabriaum <gaabrielbruck@gmail.com>
2026-03-11 00:52:43 -0300

    feat: update authentication endpoints to permit login and registration

--- src/main/java/com/gabriaum/gatekeeper/infra/security/config/SecurityConfig.java
+++ src/main/java/com/gabriaum/gatekeeper/infra/security/config/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
                             "/v3/api-docs/**",
                             "/swagger-ui.html"
                     ).permitAll()
-                    .requestMatchers("/api/v1/authentication/**").permitAll()
+                    .requestMatchers("/api/v1/authentication/login").permitAll()
+                    .requestMatchers("/api/v1/authentication/register").permitAll()
                 .anyRequest().authenticated())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint))
