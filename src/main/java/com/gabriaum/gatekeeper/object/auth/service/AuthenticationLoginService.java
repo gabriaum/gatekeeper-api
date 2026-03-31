4dc2895e4d5e40915d00a09abe599155cf247ca4
gabriaum <gaabrielbruck@gmail.com>
2026-03-31 14:31:13 -0300

    refactor: replace constructor injection with Lombok's @RequiredArgsConstructor in AuthenticationLoginService

--- src/main/java/com/gabriaum/gatekeeper/object/auth/service/AuthenticationLoginService.java
+++ src/main/java/com/gabriaum/gatekeeper/object/auth/service/AuthenticationLoginService.java
@@ -4,20 +4,14 @@ import com.gabriaum.gatekeeper.infra.service.CryptographyService;
 import com.gabriaum.gatekeeper.object.auth.dto.AuthenticationLoginRequestDTO;
 import com.gabriaum.gatekeeper.object.user.GateUser;
 import com.gabriaum.gatekeeper.object.user.repository.GateUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationLoginService {
     private final GateUserRepository gateUserRepository;
     private final CryptographyService cryptographyService;
-
-    public AuthenticationLoginService(
-            GateUserRepository gateUserRepository,
-            CryptographyService cryptographyService
-    ) {
-        this.gateUserRepository = gateUserRepository;
-        this.cryptographyService = cryptographyService;
-    }
 
     public GateUser validate(AuthenticationLoginRequestDTO dto) {
         GateUser gateUser = findUser(dto);
